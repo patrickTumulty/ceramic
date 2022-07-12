b7b6366c60871b21631327d133a862cc5fff7993
ptumulty <ptumulty1@gmail.com>
2022-07-11 22:01:33 -0700

    Added bound and unbound integer spinner component and model

--- src/main/java/com/ptumulty/ceramic/components/IntegerSpinnerComponent.java
+++ src/main/java/com/ptumulty/ceramic/components/IntegerSpinnerComponent.java
@@ -4,26 +4,42 @@ import com.ptumulty.ceramic.models.IntegerModel;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 
-public class SpinnerComponent extends UIComponent<IntegerModel, Spinner<Integer>>
+public class IntegerSpinnerComponent extends UIComponent<IntegerModel, Spinner<Integer>>
 {
-    public SpinnerComponent(IntegerModel integerModel)
+    /**
+     * Constructor
+     *
+     * @param integerModel integer spinner model
+     */
+    public IntegerSpinnerComponent(IntegerModel integerModel)
     {
         super(integerModel);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void updateModel()
     {
         model.setValue(renderer.getValue());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void initializeRenderer()
     {
         renderer = new Spinner<>();
         renderer.setEditable(true);
+        renderer.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(Integer.MIN_VALUE, Integer.MAX_VALUE, 0));
+        renderer.valueProperty().addListener((observable, oldValue, newValue) -> updateModel());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void valueChanged()
     {
