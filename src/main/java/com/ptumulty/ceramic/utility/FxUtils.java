4fa3e2ca40b154c8489cbc8d4a46e46d4b509553
ptumulty <ptumulty1@gmail.com>
2022-07-17 16:59:56 -0700

    Add initial theme

--- src/main/java/com/ptumulty/ceramic/utility/FxUtils.java
+++ src/main/java/com/ptumulty/ceramic/utility/FxUtils.java
@@ -1,6 +1,7 @@
 package com.ptumulty.ceramic.utility;
 
 import javafx.application.Platform;
+import javafx.scene.layout.Pane;
 
 public class FxUtils
 {
@@ -12,5 +13,12 @@ public class FxUtils
     public static void run(Runnable runnable)
     {
         Platform.runLater(runnable);
+    }
+
+    public static void setStaticPaneSize(Pane pane, int width, int height)
+    {
+        pane.setMinSize(width, height);
+        pane.setPrefSize(width, height);
+        pane.setMaxSize(width, height);
     }
 }
