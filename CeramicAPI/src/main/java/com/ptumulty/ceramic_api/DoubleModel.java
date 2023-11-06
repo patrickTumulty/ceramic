4d80b937614fba927625f764d694bbe18a996ea6
patrickTumulty <ptumulty1@gmail.com>
2023-11-05 23:13:01 -0800

    Working towards telemetry overlay

---
+++ CeramicAPI/src/main/java/com/ptumulty/ceramic_api/DoubleModel.java
@@ -0,0 +1,9 @@
+package com.ptumulty.ceramic_api;
+
+public class DoubleModel extends ValueModel<Double>
+{
+    public DoubleModel(Double value)
+    {
+        super(value);
+    }
+}
---
+++ CeramicAPI/src/main/java/com/ptumulty/ceramic_api/FloatModel.java
@@ -0,0 +1,9 @@
+package com.ptumulty.ceramic_api;
+
+public class FloatModel extends ValueModel<Float>
+{
+    public FloatModel(Float value)
+    {
+        super(value);
+    }
+}
