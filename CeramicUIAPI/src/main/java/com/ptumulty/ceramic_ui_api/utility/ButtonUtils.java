0ea20f7ac35ebbb894d132d95ffa6930998ced16
patrickTumulty <ptumulty1@gmail.com>
2023-12-21 23:02:33 -0800

    Fixed the control pane width issue, and also fixed the tabs hoping around a couple pixels when selected

--- CeramicUIAPI/src/main/java/com/ptumulty/ceramic_ui_api/utility/ButtonUtils.java
+++ CeramicUIAPI/src/main/java/com/ptumulty/ceramic_ui_api/utility/ButtonUtils.java
@@ -1,6 +1,7 @@
 package com.ptumulty.ceramic_ui_api.utility;
 
 import javafx.scene.control.Button;
+import javafx.scene.layout.Region;
 import javafx.scene.shape.Circle;
 import org.kordamp.ikonli.javafx.FontIcon;
 
@@ -9,10 +10,15 @@ public class ButtonUtils
     public static Button createCircleIconButton(FontIcon icon, double radius)
     {
         Button button = new Button();
+        button.setStyle("-fx-background-color: transparent");
+        button.setOnMouseEntered((event) -> button.opacityProperty().set(0.7));
+        button.setOnMouseExited((event) -> button.opacityProperty().set(1.0));
         button.setGraphic(icon);
         button.prefWidthProperty().bind(button.heightProperty());
-        button.setMinWidth(radius);
-        button.setMinHeight(radius);
+        button.setMinWidth(Region.USE_PREF_SIZE);
+        button.setMaxWidth(Region.USE_PREF_SIZE);
+        button.setMinHeight(Region.USE_PREF_SIZE);
+        button.setMaxHeight(Region.USE_PREF_SIZE);
         button.setShape(new Circle(radius));
         return button;
     }
